33c3a225adca55233c3cbdb1432b7460732568a8
David Fall <david.fall@gopuff.com>
2024-03-03 16:13:43 -0800

    Fixed all errors and set foundation for dynamically changing options for body type
    Getting weird behavior though where some items are blank. There were some instances where RGBA8A was not the source format. Might have something to do with that. Also the performance for rendering the entire tree is crap. Need to only have leaf items added/changed on expansion or when already expanded.

--- addons/lpccharactergenerator/plugin.gd
+++ addons/lpccharactergenerator/plugin.gd
@@ -4,8 +4,17 @@ extends EditorPlugin
 var loader: JSONLoader = JSONLoader.new()
 var dock: PanelContainer
 var global_tree: Tree
-	
+var body_types := ["male", "female", "teen", "child", "muscular", "pregnant"]
+var selected_body_type := "male"
+var selected_body_parts := []
+var excluded_body_parts: Dictionary = {}
+var included_body_parts: Dictionary = {}
+var none = "None"
+
 func _enter_tree():
+	for body in body_types:
+		excluded_body_parts[body] = []
+		included_body_parts[body] = []
 	var packed_scene: PackedScene = load("res://addons/lpccharactergenerator/generator_dock.tscn")
 	dock = packed_scene.instantiate()
 	ui(dock)
@@ -26,8 +35,19 @@ func ui(interface: Control):
 	
 
 func attach_next_tree_item(tree: Tree, parent: TreeItem, tree_data: Dictionary):
+	# This is parent of leaf items
 	if tree_data.has("file"):
-		attach_leaf_tree_items(tree, parent, tree_data)
+		# Get definition for body part and set metadata on parent
+		var file = tree_data["file"]
+		var definition: Dictionary = loader.load_json_file("user://sheet_definitions/" + file)
+		definition["selected_item_index"] = 0
+		parent.set_metadata(0, definition)
+		register_with_excluded(definition, parent)
+		register_with_included(definition, parent)
+		var should_hide = hide_if_excluded(definition, parent)
+		if !should_hide:
+			# Now attach leaft items to parent
+			attach_leaf_tree_items(tree, parent, definition)
 		return
 		
 	for item_text in tree_data:
@@ -36,40 +56,39 @@ func attach_next_tree_item(tree: Tree, parent: TreeItem, tree_data: Dictionary):
 		item.collapsed = true
 		item.set_text(0, item_text)
 		attach_next_tree_item(tree, item, next_tree_data)
-		return
 	return
 	
-func attach_leaf_tree_items(tree: Tree, parent: TreeItem, tree_data: Dictionary):
-	var file = tree_data["file"]
-	var definition: Dictionary = loader.load_json_file("user://sheet_definitions/" + file)
-	parent.set_metadata(0, { "selected_item_index": 0 })
-	
-	var first_item = tree.create_item(parent)
-	first_item.set_metadata(0, { "leaf": true })
-	first_item.set_cell_mode(0,TreeItem.CELL_MODE_CUSTOM)
-	first_item.set_custom_draw(0, self, "_draw_leaf_tree_item")
-	first_item.set_text(0, "None")
-	for index in range(definition["variants"].size()):
-		var title = definition["variants"][index]
-		var sheet_path = "user://spritesheets/" + definition["layer_1"]["male"] + title + ".png"
+func attach_leaf_tree_items(tree: Tree, parent: TreeItem, definition: Dictionary):
+	var variants = get_variants(definition)
+	# This is for "None" variant(i.e. no selection)
+	create_first_variant(tree, parent)
+	# This is for all variants after "None"
+	for variant in variants:
+		var item = tree.create_item(parent)
+		# Bail early if only text
+		if definition.has('use_layer_keys_for_value'):
+			print('set text for body type variant')
+			item.set_text(0, variant)
+			continue
 		
 		# Create texture of first animation for part variant
+		variant = normalize_variant(variant)
+		var sheet_path = "user://spritesheets/" + definition["layer_1"][selected_body_type] + variant + ".png"
 		var sheet = Image.load_from_file(sheet_path)
-		var image = Image.create(64,64,false,Image.FORMAT_RGBA8)
+		var sheet_format = sheet.get_format()
+		var image = Image.create(64,64,false, sheet_format)
 		image.fill(Color.WHITE)
 		image.blit_rect(sheet, image.get_used_rect() ,Vector2.ZERO)
 		var texture = ImageTexture.create_from_image(image)
 		
 		# Create leaf tree item with texture
-		var item = tree.create_item(parent)
-		item.set_metadata(0, { "sheet_path": sheet_path, "leaf": true })
+		item.set_metadata(0, { "leaf": true, "variant": variant })
 		item.set_cell_mode(0,TreeItem.CELL_MODE_CUSTOM)
 		item.set_custom_draw(0, self, "_draw_leaf_tree_item")
 		item.set_icon(0, texture)
 	return
 	
 func _draw_leaf_tree_item(item: TreeItem, rect: Rect2):
-	#print(rect)
 	# Only draw if selected leaf tree item
 	var selected_item_index = get_selected_item_index(item)
 	if selected_item_index != item.get_index():
@@ -90,13 +109,11 @@ func _draw_leaf_tree_item(item: TreeItem, rect: Rect2):
 	var white := Color(1, 1, 1)
 	global_tree.draw_rect(top_rect, white)
 	global_tree.draw_rect(bottom_rect, current_color)
-		
 
 func _on_tree_item_selected():
 	var selected_item := global_tree.get_selected()
 	if selected_item != null:
 		set_selected_item_index(selected_item)
-		
 
 func get_selected_item_index(item: TreeItem):
 	var parent := item.get_parent()
@@ -116,3 +133,41 @@ func set_selected_item_index(selected_item: TreeItem):
 		metadata = {}
 	metadata["selected_item_index"] = selected_item.get_index()
 	parent.set_metadata(0, metadata)
+	
+func normalize_variant(variant: String):
+	return variant.replace(" ", "_")
+	
+func create_first_variant(tree: Tree, parent: TreeItem):
+	var first_item = tree.create_item(parent)
+	first_item.set_metadata(0, { "leaf": true, "variant": none })
+	first_item.set_cell_mode(0,TreeItem.CELL_MODE_CUSTOM)
+	first_item.set_custom_draw(0, self, "_draw_leaf_tree_item")
+	first_item.set_text(0, none)
+
+func get_variants(definition: Dictionary):
+	# Setup body type variants if this is "body type" parent essentially
+	var variants: Array = definition["variants"]
+	if definition.has('use_layer_keys_for_value'):
+		print('create variants for body type')
+		variants = []
+		for body_type in definition["layer_1"]:
+			if body_types.has(body_type):
+				variants.push_back(body_type)
+	return variants
+	
+func register_with_included(definition: Dictionary, item: TreeItem):
+	for body_type in definition["layer_1"]:
+		if body_types.has(body_type):
+			included_body_parts[body_type].push_back(item)
+
+func register_with_excluded(definition: Dictionary, item: TreeItem):
+	for body_type in body_types:
+		if !definition["layer_1"].has(body_type):
+			excluded_body_parts[body_type].push_back(item)
+			
+func hide_if_excluded(definition: Dictionary, item: TreeItem):
+	var should_hide = false
+	if !definition["layer_1"].has(selected_body_type):
+		should_hide = true
+		item.visible = false
+	return should_hide
